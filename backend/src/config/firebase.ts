98c417a7c7f91b70b48de77e87e8dcf1da9edc21
hoangm960 <hoangm960@gmail.com>
2026-04-17 17:08:01 +0700

    add ignore undefine firebase

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -24,6 +24,7 @@ if (!admin.apps.length) {
 }
 
 export const db = admin.firestore();
+db.settings({ ignoreUndefinedProperties: true });
 export const auth = admin.auth();
 export const storage = admin.storage();
 
